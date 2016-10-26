9c9f6d059660898a1b45f9303294221e9abac814
Nat <natplane@gmail.com>
2016-10-25 22:09:57 -0400

    initial stub - ready to work!

---
+++ hellorobots.web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hellorobots.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(bool wellThatDidntWork = false)
+        {
+            ViewBag.WellThatDidntWork = wellThatDidntWork;
+
+            return View();
+        }
+
+        public ActionResult ExecuteCommand(string command)
+        {
+            switch (command)
+            {
+                case "open-the-pod-bay-doors":
+                    return 
+                        RedirectToAction("OpenThePodBayDoors");
+                default:
+                    return 
+                        RedirectToAction(
+                            "Index",
+                            new
+                            {
+                                WellThatDidntWork = true
+                            });
+            }
+        }
+
+        public ActionResult OpenThePodBayDoors()
+        {
+            return View();
+        }
+    }
+}
---
+++ hellorobots.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hellorobots.web.MvcApplication" Language="C#" %>
